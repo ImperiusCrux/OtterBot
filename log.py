c863f72597475fca5cb8ea142759d1885953e85b
Mossu <modeininger@gmx.de>
2023-03-23 12:44:03 +0100

    Bot setup and channel switch

--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from enum import Enum
 
+
 class Type(Enum):
     INFO = 0
     WARNING = 1
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import os
+import time
+import discord
+import threading
+import log
+from dotenv import load_dotenv
+from discord.ext import commands
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+client = discord.client()
+
+activeChannel = None
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+@client.event
+async def on_ready():
+    log.log(0, "Bot is ready.")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+bot = commands.Bot(command_prefix="!")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@bot.command(name="Paddel_nach")
+@commands.has_role("Enhydra lutris")
+async def setChannel(ctx, nextChannel: str):
+    global activeChannel
+    guild = ctx.guild
+    for channel in guild.channels:
+        current = channel.GuildChannel.name
+        if current is nextChannel and current is discord.ChannelType.text:
+            activeChannel = client.get_channel(current.id)
+            break
+
+    if activeChannel is None:
+        ctx.send("Hoppla, diesen Channel kann ich nicht finden.")
+        log.log(1, "Channel not found :C" )
+        return
+    await ctx.send("Okey dokey!")
+    time.sleep(1)
+    await activeChannel.send("Nett hier!")
+    log.log(0, "Connected to new Channel.")
+
+
+
+
